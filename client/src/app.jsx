fd916ed45e3d8b71caa4f1de39c8079557ed4de4
arthot <ktf.labs@gmail.com>
2018-12-22 00:29:28 +0300

    Add redux

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import todoApp from './reducers';
 
 import NavMenu from './components/NavMenu';
 import Login from './components/Login';
 import Clock from './components/Clock';
 import { UserStore, LoginContext } from './login';
+
+const store = createStore(todoApp);
 
 const App = () => (
     <div className="app-content app-content__black">
@@ -27,11 +32,13 @@ const App = () => (
 )
 
 const Root = () => (
-    <Router>
-        <UserStore>
-            <App />
-        </UserStore>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <UserStore>
+                <App />
+            </UserStore>
+        </Router>
+    </Provider>
 )
 
 export default hot(module)(Root);
