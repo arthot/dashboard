39c04cc2de6b78bf8160ccfcefb56d0ae4da3c27
arthot <ktf.labs@gmail.com>
2018-12-23 01:32:54 +0300

    Remove logs

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,4 @@ import './styles/main.scss';
 
 import App from './app.jsx';
 
-window.onerror = function(msg, url, linenumber) {
-    alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
-    return true;
-}
-
 ReactDOM.render(<App />, document.getElementById('app'));
